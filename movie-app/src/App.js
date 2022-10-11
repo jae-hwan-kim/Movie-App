861e3b8c3f85460e66b595484dd9c58044ad3dc0
김재환 <woghks7859@gmail.com>
2022-10-12 00:07:08 +0900

    FEAT: start react using CRA & making css module

---
+++ movie-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import styles from './App.module.css'
+
+function App() {
+  return (
+        <div>
+            <h1 className={styles.title}>
+              Welcome Back!!
+            </h1>
+            <Button text="i'm button"/>
+        </div>
+  );
+}
+
+export default App;
