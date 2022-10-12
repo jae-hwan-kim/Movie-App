2d46897b50852fffaa3ae37c927e3b6c44db5134
김재환 <woghks7859@gmail.com>
2022-10-12 11:42:41 +0900

    STUDY: useEffect 공부

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Button from './Button';
-import styles from './App.module.css'
+import React, { useState, useEffect } from 'react'; // eslint-disable-line no-unused-vars
 
 function App() {
-  return (
+    const [counter, setCounter] = React.useState(0);
+    const [keyword, setKeyword] = React.useState("");
+    const onClick = () => setCounter((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("I run only once");
+    }, []);
+    useEffect(() => {
+        console.log("I run when keyword changes");
+    }, [keyword])
+    useEffect(() => {
+        console.log("I run when counter changes");
+    }, [counter])
+    return (
         <div>
-            <h1 className={styles.title}>
-              Welcome Back!!
-            </h1>
-            <Button text="i'm button"/>
+            <input
+                onChange={onChange}
+                value={keyword}
+                type="text"
+                placeholder='Search'
+            />
+            <p>{counter}</p>
+            <button onClick={onClick}>Click</button>
         </div>
-  );
+    );
 }
 
 export default App;
