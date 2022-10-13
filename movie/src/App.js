ba3c8a6f93a43688ed8c258e6af9deb565ecfea8
김재환 <woghks7859@gmail.com>
2022-10-13 09:54:36 +0900

    STUDY: async & await

--- movie/src/App.js
+++ movie/src/App.js
@@ -4,22 +4,37 @@ import React, { useEffect, useState } from 'react';
 function App() {
     const [loading, setLoading] = React.useState(true);
     const [movies, setMovies] = React.useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+            )
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
     useEffect(() => {
-        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
-            .then((response) => response.json())
-            .then((json) => {
-                setMovies(json.data.movies);
-                setLoading(false);
-            });
+        getMovies();
     }, [])
     console.log(movies);
     return (
         <div>
             {loading ? <h1>is Loading...</h1>
-                : ""
-                // <select>
-                //     {movies.map((item) => <option key={item.data.movie_count}>{item.data.movie.title}</option>)}
-                // </select>
+                : 
+                <div>
+                    {movies.map((movie) => 
+                        <div key={movie.id}>
+                            <img src={movie.medium_cover_image} alt="" />
+                            <h3>{movie.title}</h3>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>)
+                    }
+                </div>
             }
         </div>
     );
