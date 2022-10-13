93395ed4c35a81f098b8bc8d36bbb23819555e5c
김재환 <woghks7859@gmail.com>
2022-10-13 14:22:51 +0900

    STUDY: react router version 6

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,43 +1,19 @@
-// eslint-disable-next-line
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from './routes/Home'
+import Detail from './routes/Detail'
 
 function App() {
-    const [loading, setLoading] = React.useState(true);
-    const [movies, setMovies] = React.useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, [])
-    console.log(movies);
-    return (
-        <div>
-            {loading ? <h1>is Loading...</h1>
-                : 
-                <div>
-                    {movies.map((movie) => 
-                        <div key={movie.id}>
-                            <img src={movie.medium_cover_image} alt="" />
-                            <h3>{movie.title}</h3>
-                            <p>{movie.summary}</p>
-                            <ul>
-                                {movie.genres.map((g) => (
-                                    <li key={g}>{g}</li>
-                                ))}
-                            </ul>
-                        </div>)
-                    }
-                </div>
-            }
-        </div>
-    );
+    return <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie" element={<Detail />} />
+        </Routes>
+    </Router>;
 }
 
 export default App;
---
+++ movie/src/components/Movie.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({coverImage, title, summary, genres}) {
+    return (
+        <div>
+            <img src={coverImage} alt={title} />
+            <h3>
+                <Link to="/movie">{title}</Link>
+            </h3>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    coverImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
