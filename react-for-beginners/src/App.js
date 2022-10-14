01b2865503e1a5d80ae95b00fda7323d4430afd6
김재환 <woghks7859@gmail.com>
2022-10-14 11:01:10 +0900

    FEAT: Donwload lecture data and match env with my env

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/hi" element={<h1>Hi</h1>} />
+            <Route path="/movie/:id" element={<Detail />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
