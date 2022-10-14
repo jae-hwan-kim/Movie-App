5d69e642142fd27bb5f4f2f447daad640b6b3640
김재환 <woghks7859@gmail.com>
2022-10-14 09:06:43 +0900

    CHORE: chore things

---
+++ build/precache-manifest.604d3b1e5444d77632289609b626f745.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "520573ca4ec5c7423d70",
+    "url": "/Movie-App/static/js/main.520573ca.chunk.js"
+  },
+  {
+    "revision": "b8bccec9e3416e963786",
+    "url": "/Movie-App/static/js/1.b8bccec9.chunk.js"
+  },
+  {
+    "revision": "a048416a66cc86643995",
+    "url": "/Movie-App/static/js/runtime~main.a048416a.js"
+  },
+  {
+    "revision": "71b1d22b7db320b47530e0bd33364320",
+    "url": "/Movie-App/index.html"
+  }
+];
