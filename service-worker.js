8ed5dfc5928ee657b37858836fbbb448cf31de35
김재환 <woghks7859@gmail.com>
2022-10-14 09:52:21 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Movie-App/index.js/precache-manifest.488b4c8d967d1108386bc8286c9a9f86.js"
+  "/Movie-App/precache-manifest.94e9db79f569a31e0c9ed2a40fb47231.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Movie-App/index.js/index.html", {
+workbox.routing.registerNavigationRoute("/Movie-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
